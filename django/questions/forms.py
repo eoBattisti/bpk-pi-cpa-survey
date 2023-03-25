d6c5daeadf6cea03186ea9999dacd52be8719224
ricardo <rtureta44@gmail.com>
2023-03-25 14:56:42 -0300

    feat:resolução de bugs 2

--- django/questions/forms.py
+++ django/questions/forms.py
@@ -6,8 +6,4 @@ class QuestionForm(forms.ModelForm):
 
     class Meta:
         model = Question
-<<<<<<< HEAD
         fields = ['description', 'axis', 'answer_type']
-=======
-        fields = ['description', 'axis', 'answer_type']
->>>>>>> 85ecdf2 (feat:resolução de bugs)
