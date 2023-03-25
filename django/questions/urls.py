37ebe072f64101bf3c18483449ca41de12ba6f68
ricardo <rtureta44@gmail.com>
2023-03-25 14:56:42 -0300

    feat:resolução de bugs 2

--- django/questions/urls.py
+++ django/questions/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from questions import views
 
 app_name = 'questions'
--- django/questions/views.py
+++ django/questions/views.py
@@ -6,11 +6,13 @@ from django.urls import reverse_lazy
 class QuestionsListView(ListView):
     model = Question
     template_name = 'list/questions_list.html'
+
 class QuestionsCreateView(CreateView):
     model = Question
     template_name = 'form/questions_editor.html'
     form_class = QuestionForm
     success_url = reverse_lazy("questions:list")
+    
 class QuestionsUpdateView(UpdateView):
     model = Question
     template_name = 'form/questions_editor.html'
