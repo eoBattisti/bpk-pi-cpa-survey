1d7f4bdb973d2a7304404856c04d8294b8e316e3
ricardo <rtureta44@gmail.com>
2023-03-25 14:22:29 -0300

    feat:Criação de cadastro de perguntas no back-end

---
+++ django/questions/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, CreateView, UpdateView
+from questions.models import Question
+from questions.forms import QuestionForm
+from django.urls import reverse_lazy
+
+class QuestionsListView(ListView):
+    model = Question
+    template_name = 'list/questions_list.html'
+
+class QuestionsCreateView(CreateView):
+    model = Question
+    template_name = 'form/questions_editor.html'
+    form_class = QuestionForm
+    success_url = reverse_lazy("questions:list")
+
+class QuestionsUpdateView(UpdateView):
+    model = Question
+    template_name = 'form/questions_editor.html'
+    form_class = QuestionForm
+    success_url = reverse_lazy("questions:list")
+
+    
