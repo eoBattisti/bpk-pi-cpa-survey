4c9c84baebbe6d7a863b8f15f828938e864e0587
camileandrade <camilefandrade@gmail.com>
2023-04-01 15:58:19 -0300

    feat: tela home e urls

--- django/biopark/urls.py
+++ django/biopark/urls.py
@@ -28,6 +28,16 @@ urlpatterns = [
     path("relatórios/professores/", views.ImportarProfessores.as_view(), name='professores')
     path("perguntas/",include('questions.urls'))
     path("perguntas/", include('questions.urls'))
+    path("perguntas/listagem/", views.PerguntasList.as_view(), name='ListagemPerguntas'),
+    path("perguntas/cadastro/", views.PerguntasCad.as_view(), name='CadastroPerguntas'),
+    path("avaliação/listagem/", views.AvaliacaoList.as_view(), name='ListagemAvaliacao'),
+    path("avaliação/criar/", views.AvaliacaoCad.as_view(), name='CriarAvaliacao'),
+    path("eixos/listagem/", views.EixosList.as_view(), name='ListagemEixos'),
+    path("eixos/cadastro/", views.EixosCad.as_view(), name='CadastroEixos'),
+    path("instituições/", views.Instituicao.as_view(), name='instituicao'),
+    path("relatórios/", views.Relatorios.as_view(), name='relatorios'),
+    path("importar/alunos/", views.ImportarAlunos.as_view(), name='alunos'),
+    path("relatórios/professores/", views.ImportarProfessores.as_view(), name='professores')
 ]
 
 
